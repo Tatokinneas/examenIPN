768243cafe3cc606906b0e85f0af3bec8d4f63fa
Octavio Benitez <tatokinneas@gmail.com>
2024-08-11 19:07:08 -0600

    Fix: gitignore config

--- src/components/Timer.jsx
+++
@@ -1,9 +0,0 @@
-import useCountdown from "../hooks/useCountdown";
-
-const Timer = ({ initialTime }) => {
-  const [timeLeft, formattedTime] = useCountdown(initialTime);
-
-  return <h2>{formattedTime}</h2>;
-};
-
-export default Timer;
