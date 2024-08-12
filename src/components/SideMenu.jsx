16e4560a05e50e0fc4b71c0082618f769e019907
Octavio Benitez <tatokinneas@gmail.com>
2024-08-11 19:09:09 -0600

    Fix: gitignore

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,13 @@
+const SideMenu = () => {
+  return (
+    <aside className="">
+      <main className="mt-10">
+        <p>Usuario</p>
+        <p>Materia: </p>
+        <p></p>
+      </main>
+    </aside>
+  );
+};
+
+export default SideMenu;
